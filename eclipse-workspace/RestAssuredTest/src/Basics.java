00c595a5f62f6a1c785c2873d7a12702bb13ba80
Md. Gofran Khan <gofran0123@gmail.com>
2023-05-25 00:50:56 +0600

    added more validation and created payload class

--- eclipse-workspace/RestAssuredTest/src/Basics.java
+++ eclipse-workspace/RestAssuredTest/src/Basics.java
@@ -1,5 +1,8 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.payload;
 
 public class Basics {
 
@@ -13,25 +16,12 @@ public class Basics {
 		
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\n"
-				+ "    \"location\" : {\n"
-				+ "        \"lat\": -38.383494,\n"
-				+ "        \"lng\": 33.427362\n"
-				+ "    },\n"
-				+ "    \"accuracy\": 50,\n"
-				+ "    \"name\" : \"Frontline house\",\n"
-				+ "    \"phone_number\" : \"(+91)9838933937\",\n"
-				+ "    \"address\": \"29, side layout, cohen 09\",\n"
-				+ "    \"types\": [\n"
-				+ "        \"shoe park\",\n"
-				+ "        \"shop\"\n"
-				+ "    ],\n"
-				+ "    \"website\":\"http://google.com\",\n"
-				+ "    \"language\": \"French_IN\"\n"
-				+ "}")
+		.body(payload.AddPlace())
 		.when().post("maps/api/place/add/json")
 		.then().log().all()
-			.assertThat().statusCode(200);
+			.assertThat().statusCode(200)
+			.body("scope", equalTo("APP"))
+			.header("Server", "Apache/2.4.41 (Ubuntu)");
 		
 	}
 
